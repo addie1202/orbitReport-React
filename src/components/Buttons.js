6b010dae5a3b51d75bd573f12106f7e8a3a67a6a
Adlai Gaither-Ganim <AdlaiGG@ADDIEs-MacBook-Pro.local>
2023-07-19 19:18:04 -0500

    worked on buttons and table

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,20 @@
-const Buttons = () => {
+import satData from "./components/satData";
+
+const Buttons = (filterByType, setSat, displaySats) => {
+  {displaySats.map((sat, id) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+    </div>
   );
+})}
+return(
+  <button onClick={() => setSat(satData)}>
+    All Orbits
+    </button>
+)
 };
 
 export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,27 @@
 const Table = ({ sat }) => {
+  let status
+  if (data.operational === true) {
+    status = "active"
+  } else {
+    status = "inactive"
+  }
+
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
+        <tr key={id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.status}</td>
         </tr>
         </tbody>
       </table>
